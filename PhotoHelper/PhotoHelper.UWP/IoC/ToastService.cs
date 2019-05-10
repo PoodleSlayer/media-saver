c6af1d9fdf1fb17e2fa5e9797ae36462d6bb302d
Chris <sgt.masterchief2552@gmail.com>
2019-05-10 01:45:37 -0400

    Toasts
    Made a basic Toast implementation on iOS and UWP. Probably wanna make them better but it's good enough for now.

--- PhotoHelper/PhotoHelper.UWP/IoC/ToastService.cs
+++ PhotoHelper/PhotoHelper.UWP/IoC/ToastService.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
 
 namespace PhotoHelper.UWP.IoC
 {
@@ -11,7 +16,36 @@ namespace PhotoHelper.UWP.IoC
 	{
 		public void MakeToast(string message)
 		{
-			// not sure how best to toast on UWP either...
+			// we want a small popup similar to how toasts work in Android
+			// use flyout for now, look into Popup later
+			var label = new TextBlock
+			{
+				Text = message,
+				Foreground = new SolidColorBrush(Colors.White),
+				HorizontalAlignment = HorizontalAlignment.Center,
+				VerticalAlignment = VerticalAlignment.Center,
+			};
+
+			var style = new Style { TargetType = typeof(FlyoutPresenter) };
+			style.Setters.Add(new Setter(Control.BackgroundProperty, new SolidColorBrush(Colors.Black)));
+			style.Setters.Add(new Setter(FrameworkElement.MaxHeightProperty, 1));
+
+			var flyout = new Flyout
+			{
+				Content = label,
+				Placement = FlyoutPlacementMode.Bottom,
+				FlyoutPresenterStyle = style,
+			};
+
+			flyout.ShowAt(Window.Current.Content as FrameworkElement);
+
+			var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(3) };
+			timer.Tick += (sender, e) =>
+			{
+				timer.Stop();
+				flyout.Hide();
+			};
+			timer.Start();
 		}
 	}
 }
--- PhotoHelper/PhotoHelper.iOS/IoC/ToastService.cs
+++ PhotoHelper/PhotoHelper.iOS/IoC/ToastService.cs
@@ -11,9 +11,35 @@ namespace PhotoHelper.iOS.IoC
 {
 	public class ToastService : IToastService
 	{
+		//NSTimer alertDelay;
+		//UIAlertController alert;
+
 		public void MakeToast(string message)
 		{
-			// how am i gonna toast on iOS oh gosh
+			// just want a quick popup like Android Toasts
+			UIAlertController alert = UIAlertController.Create(null, message, UIAlertControllerStyle.Alert);
+
+			NSTimer alertDelay = NSTimer.CreateScheduledTimer(3.0, (obj) =>
+			{
+				// passing a new alert and alertDelay each time doesn't seem ideal,
+				// maybe think of a better way to do this. probably one alert with a 
+				// cancellation token or something similar
+				DismissMessage(alert, obj);
+			});
+
+			UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+		}
+
+		private void DismissMessage(UIAlertController alert, NSTimer alertDelay)
+		{
+			if (alert != null)
+			{
+				alert.DismissViewController(true, null);
+			}
+			if (alertDelay != null)
+			{
+				alertDelay.Dispose();
+			}
 		}
 	}
 }
